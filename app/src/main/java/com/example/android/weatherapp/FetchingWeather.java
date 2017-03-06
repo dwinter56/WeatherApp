1b4121c32ceae9f3a0c1894a8065bbe0c201dd62
dwinter56 <dwinter@student.sjcny.edu>
2017-03-06 13:47:00 -0500

    adding weather api

---
+++ app/src/main/java/com/example/android/weatherapp/FetchingWeather.java
@@ -0,0 +1,18 @@
+package com.example.android.weatherapp;
+
+
+public class FetchingWeather {
+
+//declare api url as a string
+   // private String url =
+     //       "http://api.openweathermap.org/data/2.5/forecast?q=Patchogue,us&cnt=7&appid=c3368eff18484472b806c8fbdf3df950";
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/android/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/android/weatherapp/MainActivity.java
@@ -6,6 +6,14 @@ import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URL;
+
+
 public class MainActivity extends AppCompatActivity { //begin main
 
     // COMPLETED (1) Create a field to store the today display TextView
@@ -14,6 +22,63 @@ private TextView todayTextView;
     private TextView tomorrowTextView;
     private TextView thursdayTextView;
 
+    private TextView patchogueTextView; //textview to hold api
+
+    //assign values
+    String now = "45*";
+    String today = "56*/40*";
+    String tomorrow = "70*/40*";
+    String thursday = "45*/42*";
+
+String patchogue;
+
+    //USING A WEATHER API
+
+    //calling/getting the url
+    public BufferedReader getURL() throws IOException {
+        //declare api url as a string
+        String url = "http://api.openweathermap.org/data/2.5/forecast?q=Patchogue,us&cnt=7&appid=c3368eff18484472b806c8fbdf3df950";  //placing the url in a string
+        BufferedReader input = new BufferedReader(new InputStreamReader(new URL(url).openStream(), "UTF-8"));
+       // url=input.readLine();
+        return input;
+
+    }
+
+    //String patchogue = IOUtils.toString(url);
+
+   // public void parseURL(BufferedReader input)
+    //{
+     //
+   // }
+
+
+    //getting the string from the input stream...
+    public static String getStringFromInputStream(InputStream stream) throws IOException
+    {
+        int n = 0;
+        char[] buffer = new char[1024 * 4];
+        InputStreamReader reader = new InputStreamReader(stream, "UTF8");
+        StringWriter writer = new StringWriter();
+        while (-1 != (n = reader.read(buffer))) writer.write(buffer, 0, n);
+        return writer.toString();
+    }
+
+
+//setting stream to patchogue variable
+    public void setStringFromInputStream(String writer)
+    {
+       patchogue=writer.toString();
+       // patchogueTextView.append(patchogue);
+    }
+
+
+    public String getPatchogue(){
+        return patchogue;
+    }
+
+
+
+
 
 
     @Override
@@ -21,6 +86,8 @@ private TextView todayTextView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //adding an underline to the heading
         String heading="Weather App";
         TextView weatherAppText=(TextView)findViewById(R.id.weather_text);
         SpannableString spanString = new SpannableString(heading);
@@ -34,29 +101,44 @@ private TextView todayTextView;
         todayTextView = (TextView) (findViewById(R.id.today_text));
      tomorrowTextView = (TextView) (findViewById(R.id.tomorrow_text));
      thursdayTextView = (TextView) (findViewById(R.id.thursday_text));
+patchogueTextView=(TextView)(findViewById(R.id.patchogue_text));
 
 
-//assign values
-        String now = "45*";
-        String today = "56*/40*";
-        String tomorrow = "70*/40*";
-        String thursday = "45*/42*";
+
+
+
 
 
 //append the strings to the textviews
 
-
-
-
-
-   nowTextView.append(now);
-   todayTextView.append(today);
+        nowTextView.append(now);
+        todayTextView.append(today);
         tomorrowTextView.append(tomorrow);
         thursdayTextView.append(thursday);
+patchogueTextView.append(patchogue);
+
+
+
+
+
+
+
+
+
+
 
 
 
 } //end onCreate
+
+
+
+
+
+
+
+
+
 
     } //end main
 
